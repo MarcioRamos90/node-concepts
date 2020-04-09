b01d68ad6e76a825d43ccb0d9b61428063ab251b
Marcio Ramos <marcioalvesramos@hotmail.com>
2020-04-09 01:00:49 -0300

    Added functionality to endpopints

--- src/app.js
+++ src/app.js
@@ -1,33 +1,69 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const repositories = [];
+const repositories = []
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
+app.get('/repositories', (request, response) => {
+  response.status(200).json(repositories)
+})
 
-app.post("/repositories", (request, response) => {
-  // TODO
-});
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+  repositories.push(repo)
+  response.status(201).json(repo)
+})
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+app.put('/repositories/:id', (request, response) => {
+  const { title, url, techs } = request.body
+  const { id } = request.params
+  const repo = repositories.find((i) => i.id === id)
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
-});
+  if (repo) {
+    repo.title = title
+    repo.url = url
+    repo.techs = techs
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+    response.status(201).json(repo)
+  } else {
+    response.status(400).send()
+  }
+})
 
-module.exports = app;
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params
+  const index = repositories.findIndex((i) => i.id === id)
+
+  if (index >= 0) {
+    repositories.splice(index, 1)
+    response.status(204).send()
+  } else {
+    response.status(400).send()
+  }
+})
+
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params
+  const repo = repositories.find((i) => i.id === id)
+
+  if (repo) {
+    repo.likes += 1
+    response.status(201).json(repo)
+  }
+  response.status(400).send()
+})
+
+module.exports = app
